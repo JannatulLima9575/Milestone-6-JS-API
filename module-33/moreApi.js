885278744bfadc16bfbd34267d90b190d7c118a5
Jannatul <limajannat1675@gmail.com>
2025-03-10 06:26:46 +0600

    added moreApi

---
+++ module-33/moreApi.js
@@ -0,0 +1,16 @@
+const handLoadUser = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        displayUser(data)
+        
+    })
+}
+
+const displayUser = (users) => {
+    console.log(users);
+    console.log(users[0]);
+    console.log(users[4]);
+    
+}
