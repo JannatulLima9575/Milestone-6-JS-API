367f6f5015d4ed2676386a907dff31b2b0f5d6fa
Jannatul <limajannat1675@gmail.com>
2025-03-10 20:14:30 +0600

    added post json

---
+++ module-33/post.js
@@ -0,0 +1,25 @@
+const handlePost = () => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(request => request.json())
+    .then(data => {
+        displayPost(data)
+    }) 
+}
+
+const displayPost = (posts) => {
+    const postContainer = document.getElementById("postContainer");
+
+    for (const post of posts) {
+        console.log(post);
+        
+        const div = document.createElement("div");
+        div.classList.add("card")
+        div.innerHTML = `
+        <h1>${post.title}</h1>
+        <p>${post.body}</p>
+        <button>details</button>
+        `;
+        postContainer.appendChild(div)
+    }
+}
+handlePost()
