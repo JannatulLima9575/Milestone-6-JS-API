00b253381008d2d999c9033a67e20a5b3650d13f
Jannatul <limajannat1675@gmail.com>
2025-03-12 05:29:53 +0600

    added even-loop.js

---
+++ module-34/event-loop.js
@@ -0,0 +1,26 @@
+function personal() {
+    console.log('Tea Ready!');
+}
+function personal1() {
+    console.log('Jhalmuri Ready!');
+}
+function personal2() {
+    setTimeout(() => {
+        console.log('Fuchka Ready!');
+    }, 2000)
+    
+}
+function personal3() {
+    console.log('Jhalmuri Ready!');
+}
+function personal4() {
+    setTimeout(() => {
+        console.log('Fuchka Ready!');
+    }, 2000)
+}
+
+personal();
+personal1()
+personal2()
+personal3()
+personal4()
