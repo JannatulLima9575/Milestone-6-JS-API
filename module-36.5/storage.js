6f94b86d53de5cbffcfac8b85158e144180ef824
Jannatul <limajannat1675@gmail.com>
2025-03-18 01:22:15 +0600

    added json format

--- module-36.5/storage.js
+++ module-36.5/storage.js
@@ -1,5 +1,29 @@
-const age = 22;
+// const age = 22;
 
-// setItem method
-localStorage.setItem("My age is:", age);
-console.log(localStorage.getItem("My age is:"));
+// // setItem method
+// localStorage.setItem("My age is:", age);
+// console.log(localStorage.getItem("My age is:"));
+
+// getItem method
+
+const handleAddToStorage = () => {
+    // const name = "Jannatul Lima";
+    // localStorage.setItem("name", name);
+    // localStorage.getItem("name");
+
+    // try Object
+    const person = {
+        name : "Lima",
+        age : 22,
+        friends: ["Ruba", "Tamanna"],
+    }
+    const convertedToString = JSON.stringify(person);
+    localStorage.setItem("person", convertedToString);
+    
+    // return;
+}
+handleAddToStorage()
+
+    // try Object
+
+
